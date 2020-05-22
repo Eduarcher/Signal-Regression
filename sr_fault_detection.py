9e1febb56506d1b0d75a22a73ee72265a68caf32
eduardo <eduardo12bg@gmail.com>
2020-05-22 16:53:09 -0300

    Added optimization and plotting

--- sr_fault_detection.py
+++ sr_fault_detection.py
@@ -13,8 +13,24 @@ from scipy.optimize import minimize
 #https://towardsdatascience.com/understanding-random-forest-58381e0602d2
 
 
-def noise(amplitude, count):
-    return np.random.normal(0, amplitude, count)
+def gen_parametric_function_data(fun, timeline):
+    return fun(timeline)
+
+
+def noise(std, count):
+    return np.random.normal(0, std, count)
+
+
+def gen_validation_data(fun, start, end, points=1000, nstd=0.1, fault_count=50):
+    random_start = rd.randint(start, end)
+    tl = np.linspace(random_start, random_start + start, points)
+    data = gen_parametric_function_data(fun, tl) + noise(nstd, points)
+    labels = np.zeros(points)
+    for i in range(fault_count):
+        random_fault = rd.randint(0, points - 1)
+        labels[random_fault] = 1.
+        data[random_fault] *= np.random.uniform(-10, 10)
+    return tl, data, labels
 
 
 def calc_labels(limiar, recs, reals):
@@ -37,20 +53,28 @@ def calc_labels_fitness(limiar, metric='acc'):
     elif metric == 'precision':
         return -(cm[1][1]/(cm[1][0] + cm[1][1]))
     elif metric == 'f1':
-        return -(2*((cm[1][1]/(cm[1][0] + cm[1][1]))*(cm[1][1]/(cm[0][1] + cm[1][1]))) / ((cm[1][1]/(cm[1][0] + cm[1][1]))+(cm[1][1]/(cm[0][1] + cm[1][1]))))
+        return -(2*((cm[1][1]/(cm[1][0] + cm[1][1]))*(cm[1][1]/(cm[0][1] + cm[1][1]))) /
+                 ((cm[1][1]/(cm[1][0] + cm[1][1]))+(cm[1][1]/(cm[0][1] + cm[1][1]))))
 
+
+def fun(x):
+    # return np.sin(x) + np.cos(x) ** 2
+    return 5*(x - np.floor(x))
 
 # Params
-noise_std = 0.1
-end = 100
-points_count = 1000
-validation_max_end = 1000
-validation_fault_count = 50
+parametric_function = fun
+noise_std = 0.01
+points_count = 5000
+timeline_end = 25
+validation_max_end = timeline_end + 1000
+validation_fault_count = int(0.03 * points_count)
 initial_fault_limiar = 0.2
 
 # Timeline and original base function
-timeline = np.linspace(0, end, points_count)
-sample_data = np.sin(timeline) + noise(noise_std, points_count)
+timeline = np.linspace(0, timeline_end, points_count)
+sample_data = gen_parametric_function_data(parametric_function, timeline) + noise(noise_std, points_count)
+plt.plot(timeline, sample_data)
+plt.show()
 
 # Train/Test separation
 X_train, X_test, y_train, y_test = train_test_split(timeline, sample_data, test_size=0.2)
@@ -68,17 +92,23 @@ score = reg.score(X_test.reshape(-1, 1), y_test)
 print("Function Regressed:", reg._program, " | Score:", score)
 
 # Create validation data with fault and labels
-random_start = rd.randint(end, validation_max_end)
-validation_timeline = np.linspace(random_start, random_start + end, points_count)
-validation_data = np.sin(validation_timeline) + noise(noise_std, points_count)
-real_labels = np.zeros(points_count)
-for i in range(validation_fault_count):
-    random_fault = rd.randint(0, points_count-1)
-    real_labels[random_fault] = 1.
-    validation_data[random_fault] *= np.random.uniform(-10, 10)
+validation_timeline, validation_data, real_labels = gen_validation_data(parametric_function,
+                                                                        timeline_end,
+                                                                        validation_max_end,
+                                                                        points_count,
+                                                                        noise_std,
+                                                                        validation_fault_count)
 
 # Validate fault detection
 reconstruction = reg.predict(validation_timeline.reshape(-1, 1))
 res = minimize(calc_labels_fitness, np.array([initial_fault_limiar]), method='Nelder-Mead')
 rec_labels = calc_labels(res.x[0], reconstruction, validation_data)
 print(confusion_matrix(real_labels, rec_labels))
+
+# Plot results
+fig, axs = plt.subplots(1, 2, sharey=True)
+fig.set_figheight(6)
+fig.set_figwidth(10)
+# axs[0].plot(validation_timeline, validation_data)
+axs[1].plot(validation_timeline, reconstruction)
+fig.show()
